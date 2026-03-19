710f6e51ccbbea5dbe8e21babc994b9be123c851
tadblair <90925265+tadblair@users.noreply.github.com>
2026-03-19 15:45:35 -0700

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Colina-Reserve-Transfer-Explorer/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
